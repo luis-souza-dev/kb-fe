450b59f70b7f6bbf748ca7be297aefc9af59bfa1
Luis <luisotavioldesouza1+1@gmail.com>
2023-08-23 16:55:01 +0100

    feat: base home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Main from "./layout/main";
+import Carousel from "./layout/carousel";
+import ArticlesList from "./layout/articles-list";
+import Header from "./layout/header";
+
 function App() {
+  const categories = Array.from({length: 16}, (v, i) => i);
   return (
-    <div>
-      
-    </div>
+    <Main>
+      <Header></Header>
+      <Carousel categories={categories}></Carousel>
+      <ArticlesList></ArticlesList>
+    </Main>
   );
 }
 
