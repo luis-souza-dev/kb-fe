c6b98272b626c2da83f4fc51b7652bb8d1a0572b
Luis <luisotavioldesouza1+1@gmail.com>
2023-08-23 16:53:21 +0100

    feat: tailwind custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '7': 'repeat(7, minmax(0, 1fr))',
+        '8': 'repeat(8, minmax(0, 1fr))',
+        '9': 'repeat(9, minmax(0, 1fr))',
+        '10': 'repeat(10, minmax(0, 1fr))',
+      },
+      gridRow: {
+        'span-7': 'span 7 / span 7',
+        'span-8': 'span 8 / span 8',
+        'span-9': 'span 9 / span 9',
+        'span-10': 'span 10 / span 10',
+      },
+      colors: {
+        'main-borders': '#cec7c7',
+      },
+    },
   },
   plugins: [],
 }
