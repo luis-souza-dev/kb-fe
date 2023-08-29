3499205cd113fa50fa2919090f7198181b2685dc
Luis <luisotavioldesouza1+1@gmail.com>
2023-08-29 18:52:55 +0100

    feat: updating styles and adding category pill to list item

--- src/components/category-card/index.js
+++ src/components/category-card/index.js
@@ -1,6 +1,6 @@
 const CategoryCard = (props) => {
     return (
-      <div className="border-main-borders border rounded-md w-36 h-36 shadow-sm shadow-main-shadows">
+      <div className="border-main-borders border rounded-md w-36 h-36 shadow-md shadow-main-shadows cursor-pointer">
         { props.name }
         { props.icon }
       </div>
--- src/components/list-item/index.js
+++ src/components/list-item/index.js
@@ -1,10 +1,26 @@
+import CategoryPill from "../category-pill";
+
 const ListItem = (props = { isFirst: false }) => {
     const borderClasses = 'border-t border-main-borders'
     return (
-        <div className={`w-full h-16 ${props.isFirst ? "" : borderClasses}`}>
-            Title
-            Categoriessssss
-            Keywords
+        <div className={`w-full h-20 flex flex-wrap cursor-pointer p-2 ${props.isFirst ? "" : borderClasses}`}>
+            <div className="w-full text-xl flex gap-x-4 items-start">
+                <span> Title </span>
+                <div className="flex gap-x-1 h-[60%] my-1">
+                    <CategoryPill>
+                        GO
+                    </CategoryPill>
+                    <CategoryPill>
+                        BackEnd
+                    </CategoryPill>
+                    <CategoryPill>
+                        Automation
+                    </CategoryPill>
+                </div>
+            </div>
+            <span className="w-full text-sm text-[#808992]">
+                Keywords
+            </span>
         </div>
     )
 }
