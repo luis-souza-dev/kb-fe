4140a9c1f206304b71e305f1fb3ca1c23c0cc326
Luis <luisotavioldesouza1+1@gmail.com>
2023-08-29 20:09:28 +0100

    feat: implementing initial search bar structure

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,6 +1,8 @@
+import SearchInput from "../../components/search-input";
+
 const Header = () => (
-    <header className="col-span-full row-span-1 border-b border-t-0 border-main-borders">
-        Headd
+    <header className="col-span-full row-span-1 border-b border-t-0 border-main-borders flex items-center justify-center">
+        <SearchInput> </SearchInput>
     </header>
 )
 
